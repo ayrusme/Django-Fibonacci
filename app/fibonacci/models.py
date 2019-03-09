a21e39375ea694b9573a51a8be87e45430d0e5fd
Surya Raman <surya@yellowmessenger.com>
2019-03-09 22:29:10 +0530

    Reduce to single function

--- app/fibonacci/models.py
+++ app/fibonacci/models.py
@@ -5,11 +5,8 @@ from django.db import models
 
 
 # Create your models here.
-def fibonacci(number):
-    phi = (1 + sqrt(5)) / 2
-    return round(pow(phi, number) / sqrt(5))
-
 
 def n_fibonacci(number):
     number = int(number)
-    return fibonacci(number)
+    phi = (1 + sqrt(5)) / 2
+    return round(pow(phi, number) / sqrt(5))
