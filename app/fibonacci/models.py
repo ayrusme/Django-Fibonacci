08ab6272aa4087751de0281f5b096dc0654f8b48
Surya Raman <surya@yellowmessenger.com>
2019-03-09 23:04:38 +0530

    Add elapsed time

--- app/fibonacci/models.py
+++ app/fibonacci/models.py
@@ -1,12 +1,14 @@
 from math import pow, sqrt
-from time import time
+from timeit import default_timer
 
 from django.db import models
-
 
 # Create your models here.
 
 def n_fibonacci(number):
+    start = default_timer()
     number = int(number)
     phi = (1 + sqrt(5)) / 2
-    return round(pow(phi, number) / sqrt(5))
+    result = round(pow(phi, number) / sqrt(5))
+    end = default_timer()
+    return result, end-start
--- app/fibonacci/views.py
+++ app/fibonacci/views.py
@@ -10,7 +10,8 @@ def number(request):
         form = InputField(request.POST)
         if form.is_valid():
             number = form.cleaned_data['number']
-            return HttpResponse(n_fibonacci(number))
+            response, time_taken = n_fibonacci(number)
+            return HttpResponse('The result is {} \n The time taken is {} seconds'.format(response, time_taken))
         else:
             return HttpResponse("That doesn't look like a number!")
     elif request.method == "GET":    
